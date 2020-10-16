6c17e4c7ee5b863861fe09f716d73ef94de7db2b
Kakk22 <1029839308@qq.com>
2020-10-16 23:05:10 +0800

    init project

---
+++ src/test/java/com/cyf/like/LikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyf.like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
